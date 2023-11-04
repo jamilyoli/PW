fb0fade13c506938e106d50ff90a854cba0dc47e
jamilyoli <112913130+jamilyoli@users.noreply.github.com>
2023-11-04 12:50:50 -0300

    Delete oficial/conexaoReview.php

--- oficial/conexaoReview.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $dbHost = 'Localhost';
-   $dbUsername = 'root';
-   $dbPassword = '';
-   $dbName = 'upload';
-
-   $conexaor = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-?>
