b91d9437c706376f9fa6ea2f751c833e1dd32337
jamilyoli <112913130+jamilyoli@users.noreply.github.com>
2023-11-04 12:51:26 -0300

    Delete oficial/config.php

--- oficial/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $dbHost = 'Localhost';
-   $dbUsername = 'root';
-   $dbPassword = '';
-   $dbName = 'cadastro';
-
-   $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-   
-?>
