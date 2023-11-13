4c04b07a93e9c3986f04702021b0c0042cd80077
jamilyoli <112913130+jamilyoli@users.noreply.github.com>
2023-11-12 21:38:28 -0300

    Update formulario_review.php

--- oficial/PHP/formulario_review.php
+++ oficial/PHP/formulario_review.php
@@ -7,13 +7,27 @@ if (isset($_POST['submit'])){
     $autor = $_POST['autor'];
     $npaginas = $_POST['nPaginas'];
     $descri = $_POST['descEntrada'];
-    $avalia = $_POST['avaliacao'];
 
-    move_uploaded_file($file_image, 'file/'.$image);
-    $result = mysqli_query($conexaor, "INSERT INTO tbl_upload(titulo, image, autor, nPaginas, avaliacao, descricao) 
-    VALUES ('$titulo', '$image', '$autor', '$npaginas', '$avalia','$descri')");
-    header('Location: lista_review.php');
+    if (!empty($_POST['avaliacao'])){
+        $avalia = $_POST['avaliacao'];
+
+        $result_avaliacoes = mysqli_query($conexaor, "INSERT INTO avaliacoes (qnt_estrela, created) VALUES ('$avalia', NOW())");
+
+        if ($result_avaliacoes) {
+            move_uploaded_file($file_image, 'file/' . $image);
+            $result = mysqli_query($conexaor, "INSERT INTO tbl_upload(titulo, image, autor, nPaginas, avaliacao, descri) 
+                VALUES ('$titulo', '$image', '$autor', '$npaginas', '$avalia', '$descri')");
+
+            if ($result) {
+                header('Location: lista_review.php');
+            } else {
+                echo "Erro ao inserir dados na tabela tbl_upload: " . mysqli_error($conexaor);
+            }
+        } else {
+            echo "Erro ao inserir dados na tabela avaliacoes: " . mysqli_error($conexaor);
+        }
+    } else {
+        echo "Avaliação não foi selecionada.";
+    }
 }
-    
-        
 ?>
